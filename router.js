a5d5973271ee32d6479da4cb272d3c997dbe12bb
HerrSubset <smets.pj@gmail.com>
2017-09-04 19:05:16 +0200

    Store routes in array
    Later on, the order in which routes are defined will become
    important, so they should be stored in an array, not in an
    object where the order of the keys can't be controlled.

--- router.js
+++ router.js
@@ -1,13 +1,19 @@
-const routes = {}
+const routes = []
 
 
 module.exports = {
-    add: (route, callback) => routes[route] = callback,
+    add: (route, callback) => routes.push({route: route, callback: callback}),
+
     dispatch: (req, res) => {
         const { url } = req
         console.log(`Incoming request on ${url}`)
-        if (routes[url]) {
-            routes[url](req, res) 
+
+        const route = routes
+            .filter(x => x.route === url)
+            .shift()
+
+        if (route) {
+            route.callback(req, res) 
         } else {
             res.statusCode = 404
             res.end('Oops, 404 not found')
