f20e2fa0634fe158916fcfe1e7b4f1851f69f7f4
HerrSubset <smets.pj@gmail.com>
2017-09-06 21:10:03 +0200

    set up mocha testing with chai asserts

---
+++ test/test.spec.js
@@ -0,0 +1,8 @@
+const { expect } = require('chai')
+const router = require('../router.js')
+
+describe('A dummy test', () => {
+    it('should be true', () => {
+        expect(true).to.equal(true)
+    })
+})
